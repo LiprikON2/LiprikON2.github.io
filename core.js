0301263e6ae0033c828de8ec6580ce94036dbbf5
LiprikON <kpyton2@gmail.com>
2019-02-26 23:46:32 +0300

    Website upload

---
+++ core.js
@@ -0,0 +1,17 @@
+
+const timeLeft = document.querySelector('.timer');
+
+
+
+function renderTime() {
+    let now = new Date();
+
+    // Formating time to 00 format (9 -> 09)
+    hour = ("0" +now.getHours()).slice(-2);
+    minute = ("0" +now.getMinutes()).slice(-2);
+    second = ("0" +now.getSeconds()).slice(-2);
+
+    timeLeft.innerHTML = `${hour}:${minute}:${second}`;
+}
+
+setInterval(renderTime, 1000)
