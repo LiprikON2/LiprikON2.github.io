b5b59f881a99295b6579419d40edfa44f334ff05
LiprikON <kpyton2@gmail.com>
2019-03-01 16:34:22 +0300

    Update 15
    Adjust x2.8 every sidebar value to correlate to zoom value of 35%.
    (original source https://www.w3schools.com/howto/howto_js_sidenav.asp)

--- script/core.js
+++ script/core.js
@@ -25,7 +25,7 @@ menu.addEventListener('click', () => console.log('haha'));
 
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "700px";
 }
 
 /* Set the width of the side navigation to 0 */
