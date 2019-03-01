ae62a5765766c2645241c662e3b90835204da455
LiprikON <kpyton2@gmail.com>
2019-03-01 16:15:59 +0300

    Update 13
    Added sidebar

--- script/core.js
+++ script/core.js
@@ -21,3 +21,14 @@ setInterval(renderTime, 1000)
 const menu = document.querySelector('#menu');
 
 menu.addEventListener('click', () => console.log('haha'));
+
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
