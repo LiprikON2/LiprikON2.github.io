e317827493e585ed2f32a1ff3d3d41160081e41d
LiprikON <kpyton2@gmail.com>
2019-06-02 15:17:02 +0300

    Update 27
    .closeSidenav fix

--- script/core.js
+++ script/core.js
@@ -61,11 +61,13 @@ function goMobile() {
 /* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "700px";
+    document.querySelector(".closeSidenav").style.display = "unset";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.querySelector(".closeSidenav").style.display = "none";
 }
 
 
